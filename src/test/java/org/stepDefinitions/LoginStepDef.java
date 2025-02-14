855bf23b88c299e1efa85d0abf6d8e64c227ac75
Saumya <singhsaumyait034@gmail.com>
2025-02-14 20:08:20 +0530

    Step Definition Skeleton

---
+++ src/test/java/org/stepDefinitions/LoginStepDef.java
@@ -0,0 +1,52 @@
+package org.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDef {
+
+    @Given("user should be on OpenCart Application")
+    public void user_should_be_on_open_cart_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("user has enter valid username and password")
+    public void user_has_enter_valid_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user click on login button")
+    public void user_click_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should be logged in successfully")
+    public void user_should_be_logged_in_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @Given("user entered invalid {string} and {string}")
+    public void user_entered_invalid_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should get error message {string}")
+    public void user_should_get_error_message(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user click on {string} link")
+    public void userClickOnLink(String linkName) {
+    }
+
+    @Then("user should be redirected to the password reset page")
+    public void userShouldBeRedirectedToThePasswordResetPage() {
+    }
+}
